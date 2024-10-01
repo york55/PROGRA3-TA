19a8537e3fd9f9c69235bf011435ce7f6a11cd1b
aaron <aaron@Aaron>
2024-10-01 17:38:41 -0500

    LISTAR PARA RRHH, ERROR EN LISTARADMIN

--- Proyectos/CreditoMovil/src/pe/edu/pucp/creditomovil/program/main/Principal.java
+++ Proyectos/CreditoMovil/src/pe/edu/pucp/creditomovil/program/main/Principal.java
@@ -26,27 +26,39 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Supervisor supervisor = new Supervisor(1, new Date(), "Diego", "Pérez", "Gonzalez", "miContrasena", new Date(), true, "A", 1, "SUP123");
+        Supervisor supervisor = new Supervisor(6, new Date(), "Diego", "Pérez", "Gonzalez", "miContrasena", new Date(), true, "B", 1, "SUP123");
         UsuarioDAO daoUsuario = new UsuarioMySQL();
         //daoUsuario.insertar(supervisor);
         //daoUsuario.modificar(2,supervisor);
         //daoUsuario.eliminar(3);
         List<Usuario> listaUsuarios = daoUsuario.listarTodos();
-        for (Usuario usuario : listaUsuarios) {
-            System.out.println("ID: " + usuario.getIdUsuario());
-            System.out.println("Nombre: " + usuario.getNombre());
-        }
+//        for (Usuario usuario : listaUsuarios) {
+//            System.out.println("ID: " + usuario.getIdUsuario());
+//            System.out.println("Nombre: " + usuario.getNombre());
+//        }
         SupervisorDAO daoSupervisor = new SupervisorMySQL();
         //daoSupervisor.insertar(supervisor);
         //daoSupervisor.modificar(1, supervisor);
         //daoSupervisor.eliminar(1);
+        List<Supervisor> listaSupervisor = daoSupervisor.listarTodos();
+//        for (Supervisor sup : listaSupervisor) {
+//            System.out.println("ID: " + sup.getIdUsuario());
+//            System.out.println("Nombre: " + sup.getNombre());
+//            System.out.println("codigo_sup: " + sup.getCodigoEv());
+//        }
         
         //daoUsuario.eliminar(4);
-        Administrador administrador = new Administrador(5, new Date(), "Diego", "Pérez", "Gonzalez", "miContrasena", new Date(), true, "A", 1);
+        Administrador administrador = new Administrador(6, new Date(), "Diego", "Pérez", "Gonzalez", "miContrasena", new Date(), true, "B", 2);
         AdministradorDAO daoAdministrador = new AdministradorMySQL();
         //daoAdministrador.insertar(administrador);
         //daoAdministrador.modificar(5, administrador);
         //daoAdministrador.eliminar("A");
+        List<Administrador> listaAdministrador = daoAdministrador.listarTodos();
+//        for (Administrador admin : listaAdministrador) {
+//            System.out.println("ID: " + admin.getIdUsuario());
+//            System.out.println("Codigo admin: " + admin.getCodigoAdm());
+//            System.out.println("Codigo cargo: " + admin.getCodigoCargo());
+//        }
     }
     
 }
